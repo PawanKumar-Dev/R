7e81ca32fa6e86d01977ab6f43a0df3ca158bbb2
PawanKumar-Dev <pk687376@gmail.com>
2025-03-05 12:19:40 +0530

    Update

--- 12project/src/App.jsx
+++ 12project/src/App.jsx
@@ -1,12 +1,41 @@
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components'
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>Blog App</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  if (loading) {
+    return (
+      <>
+        <Header />
+        <h1>Blog App</h1>
+        <Footer />
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h1>Something went wrong</h1>
+      </>
+    )
+  }
 }
 
 export default App
