862f2c4ddb7923b145db298571df0ed882a55c23
PawanKumar-Dev <pk687376@gmail.com>
2025-03-05 14:40:19 +0530

    Update

--- 12project/src/components/Header/Logout.jsx
+++ 12project/src/components/Header/Logout.jsx
@@ -1,9 +1,20 @@
 import { useDispatch } from "react-redux"
 import authService from "../../appwrite/auth"
+import { logout } from "../../store/authSlice"
 
 const Logout = () => {
+    const dispatch = useDispatch()
+
+    function logOutHandler() {
+        authService.logOut().then(() => {
+            dispatch(logout())
+        })
+    }
+
     return (
-        <div>Logout</div>
+        <button className="px-4 py-2 bg-red-600 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75">
+            Logout
+        </button>
     )
 }
 
